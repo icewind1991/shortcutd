6b81b14b814cd965c5c68de28b4eb419ce57c57f
Robin Appelman <robin@icewind.nl>
2020-04-11 14:32:28 +0200

    client lib

---
+++ examples/client.rs
@@ -0,0 +1,23 @@
+use shortcutd_client::{Shortcut, ShortcutClient};
+use std::error::Error;
+use std::time::Duration;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut client = ShortcutClient::new()?;
+
+    let shortcut: Shortcut = "<Ctrl><Alt>-KeyP".parse()?;
+
+    client.register(shortcut, |s| {
+        eprintln!("shortcut1 {}", s);
+    })?;
+
+    let shortcut: Shortcut = "<Ctrl><Alt>-KeyO".parse()?;
+
+    client.register(shortcut, |s| {
+        eprintln!("shortcut2 {}", s);
+    })?;
+
+    loop {
+        client.process(Duration::from_millis(1000))?;
+    }
+}
