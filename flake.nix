60465ac735dce5296fa3a5b50658d3ed4a1f9ec5
Robin Appelman <robin@icewind.nl>
2023-06-17 20:10:21 +0200

    ci

--- flake.nix
+++ flake.nix
@@ -1,83 +1,106 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:icewind1991/naersk?rev=6d245a3bbb2ee31ec726bb57b9a8b206302e7110";
-    naersk.inputs.nixpkgs.follows = "nixpkgs";
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    rust-overlay.inputs.flake-utils.follows = "utils";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
+    cross-naersk.url = "github:icewind1991/cross-naersk";
+    cross-naersk.inputs.nixpkgs.follows = "nixpkgs";
+    cross-naersk.inputs.naersk.follows = "naersk";
   };
 
   outputs = {
     self,
-    nixpkgs,
-    utils,
+    flake-utils,
+    cross-naersk,
     naersk,
+    nixpkgs,
     rust-overlay,
+    ...
   }:
-    utils.lib.eachDefaultSystem (system: let
+    flake-utils.lib.eachDefaultSystem (system: let
       overlays = [ (import rust-overlay) ];
       pkgs = (import nixpkgs) {
         inherit system overlays;
       };
       lib = pkgs.lib;
-      naerskForTarget = target: let
-        toolchain = pkgs.rust-bin.stable.latest.default.override { targets = [target]; };
-      in pkgs.callPackage naersk {
-        cargo = toolchain;
-        rustc = toolchain;
-      };
+      inherit (builtins) map replaceStrings;
+      inherit (lib.strings) hasInfix;
+      inherit (lib.attrsets) nameValuePair genAttrs listToAttrs;
+
       hostTarget = pkgs.hostPlatform.config;
-      targets = ["x86_64-pc-windows-gnu" hostTarget];
-      hostNaersk = naerskForTarget hostTarget;
-      src = lib.sources.sourceByRegex (lib.cleanSource ./.) ["Cargo.*" "(src)(/.*)?"];
-      nearskOpt = {
-        pname = "vbspview";
-        root = src;
-        nativeBuildInputs = buildDependencies;
-      };
-      crossArgs = {
-        "x86_64-pc-windows-gnu" = {
-          nativeBuildInputs = [ pkgs.pkgsCross.mingwW64.stdenv.cc ];
-          overrideMain = args: args // { buildInputs = [ pkgs.pkgsCross.mingwW64.windows.pthreads ]; };
-        };
-      };
-      crossArgsFor = target: if (hostTarget != target) then (crossArgs.${target} or {}) else {};
-      buildDependencies = with pkgs; [
-        freetype
-        pkgconfig
-        dbus
-        libGL
+      targets = [
+        "x86_64-unknown-linux-musl"
+        hostTarget
       ];
+
+      releaseTargets = lib.lists.remove hostTarget targets;
+
+      toolchain = (pkgs.rust-bin.stable.latest.default.override { inherit targets; });
+      assetNameForTarget = replaceStrings ["-unknown" "-gnu" "-musl" "eabihf" "-pc"] ["" "" "" "" ""];
+
+      cross-naersk' = pkgs.callPackage cross-naersk {inherit naersk;};
+      src = lib.sources.sourceByRegex (lib.cleanSource ./.) ["Cargo.*" "(server|client)(/.*)?"];
+      naerskOpt = {
+        pname = "shortcutd";
+        root = src;
+      };
+
+      buildMatrix = targets: {
+        include = (map (target: {
+          inherit target;
+          artifact_name = "shortcutd";
+          asset_name = "shortcutd-${assetNameForTarget target}";
+        }) targets ++ map (target: {
+          target = "${target}-example-client";
+          artifact_name = "client";
+          asset_name = "example-client-${assetNameForTarget target}";
+        }) targets);
+      };
+      serverPackages = genAttrs targets (target: (cross-naersk' target).buildPackage ({
+        pname = "shortcutd";
+        root = src;
+      }));
+      clientPackages = listToAttrs (map (target: nameValuePair "${target}-example-client" ((cross-naersk' target).buildPackage ({
+        pname = "shortcutd-example-client";
+        root = src;
+
+        overrideMain = x: {
+            preConfigure = ''
+            cargo_build_options="$cargo_build_options --example client"
+          '';
+        };
+      }))) targets);
     in rec {
-      packages = (lib.attrsets.genAttrs targets (target: (naerskForTarget target).buildPackage (nearskOpt // {
-        CARGO_BUILD_TARGET = target;
-      } // (crossArgsFor target)))) // rec {
-        vbspview = packages.${hostTarget};
-        check = hostNaersk.buildPackage (nearskOpt // {
+      packages = serverPackages // clientPackages // rec {
+        shortcutd = packages.${hostTarget};
+        example-client = packages."${hostTarget}-example-client";
+        check = (cross-naersk' hostTarget).buildPackage (naerskOpt // {
           mode = "check";
         });
-        clippy = hostNaersk.buildPackage (nearskOpt // {
+        clippy = (cross-naersk' hostTarget).buildPackage (naerskOpt // {
           mode = "clippy";
         });
-        default = vbspview;
+        default = shortcutd;
       };
 
       inherit targets;
+      inherit releaseTargets;
+      matrix = buildMatrix targets;
+      releaseMatrix = buildMatrix releaseTargets;
+
+      apps = rec {
+        tf-demo-parser = flake-utils.lib.mkApp {
+          drv = packages.tf-demo-parser;
+          exePath = "/bin/parse_demo";
+        };
+        default = tf-demo-parser;
+      };
 
       devShells.default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          pkgs.rust-bin.stable.latest.default
-          bacon
-          cargo-edit
-          cargo-outdated
-          clippy
-          cargo-audit
-          cargo-msrv
-        ] ++ buildDependencies;
-
-        LD_LIBRARY_PATH = with pkgs; "/run/opengl-driver/lib/:${lib.makeLibraryPath ([libGL libGLU])}";
+        nativeBuildInputs = with pkgs; [rust-bin.stable.latest.default bacon cargo-edit cargo-outdated rustfmt clippy cargo-audit hyperfine valgrind];
       };
     });
 }
